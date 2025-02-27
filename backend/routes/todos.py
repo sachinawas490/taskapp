fede76a67ecff7e7f4ce8efaa7030dff9e976743
w3villa-sachin-awasthi <sachin.awasthi@w3villa.com>
2025-02-27 18:19:13 +0530

    protected routed added successfully

--- backend/routes/todos.py
+++ backend/routes/todos.py
@@ -1,34 +1,39 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException,Depends
 from database import db
 from models import Todo
 from bson import ObjectId
-
+from utils import verify_token
 router = APIRouter(prefix="/todos", tags=["Todos"])
 
 # Todo Collection
 todos_collection = db["todos"]
+@router.post("/todo")
+async def create_todo(todo:Todo,users:str=Depends(verify_token)):
+    print("type of todo",type(todo.todo))
+    print("Value of the users", users)
+    return users
 
-@router.post("/")
-async def create_todo(todo: Todo):
-    user_id = ObjectId(todo.user_id)
-    new_todo = {"todo": todo.todo, "completed": todo.completed, "user_id": user_id}
-    todos_collection.insert_one(new_todo)
-    return {"message": "Todo created successfully"}
+# @router.post("/")
+# async def create_todo(todo: Todo):
+#     user_id = ObjectId(todo.user_id)
+#     new_todo = {"todo": todo.todo, "completed": todo.completed, "user_id": user_id}
+#     todos_collection.insert_one(new_todo)
+#     return {"message": "Todo created successfully"}
 
-@router.get("/{user_id}")
-async def get_todos(user_id: str):
-    user_id = ObjectId(user_id)
-    todos = list(todos_collection.find({"user_id": user_id}, {"_id": 0}))
-    return todos
+# @router.get("/{user_id}")
+# async def get_todos(user_id: str):
+#     user_id = ObjectId(user_id)
+#     todos = list(todos_collection.find({"user_id": user_id}, {"_id": 0}))
+#     return todos
 
-@router.put("/{todo_id}")
-async def update_todo(todo_id: str, todo: Todo):
-    todo_id = ObjectId(todo_id)
-    todos_collection.update_one({"_id": todo_id}, {"$set": {"todo": todo.todo, "completed": todo.completed}})
-    return {"message": "Todo updated successfully"}
+# @router.put("/{todo_id}")
+# async def update_todo(todo_id: str, todo: Todo):
+#     todo_id = ObjectId(todo_id)
+#     todos_collection.update_one({"_id": todo_id}, {"$set": {"todo": todo.todo, "completed": todo.completed}})
+#     return {"message": "Todo updated successfully"}
 
-@router.delete("/{todo_id}")
-async def delete_todo(todo_id: str):
-    todo_id = ObjectId(todo_id)
-    todos_collection.delete_one({"_id": todo_id})
-    return {"message": "Todo deleted successfully"}
+# @router.delete("/{todo_id}")
+# async def delete_todo(todo_id: str):
+#     todo_id = ObjectId(todo_id)
+#     todos_collection.delete_one({"_id": todo_id})
+#     return {"message": "Todo deleted successfully"}
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import { url } from '../utils/data';
+export default function Home() {
+    const [todo, setTodo] = useState('')
+    const navigate=useNavigate();
+    const token=localStorage.getItem('userToken')
+    async function handleClick(){
+        if(!token){
+            navigate('/')
+        }else{
+            let data={todo}
+            console.log(data)
+            const response=await axios.post(`${url}/todos/todo`,data,{
+                headers:{
+                    Authorization:`Bearer ${token}`
+                }
+            })
+            console.log(response)
+        }
+    }
+    return (
+        <div className='bg-slate-50 text-black'>
+            <h2 className='text-[40px] text-center mt-8 bg-slate-200 py-4 rounded-xl mx-5'>Todo app</h2>
 
-function Home() {
-  return (
-    <div>Home</div>
-  )
+            <div className="flex  flex-col border-2 border-slate-950 mx-5 mt-5 min-h-[200px] items-center ">
+                <input type="text"
+                name='todo'
+                value={todo}
+                onChange={e=>setTodo(e.target.value)}
+                className='text-black w-[50%] border-2 border-slate-900 p-3 rounded-md mt-5' placeholder='enter your todo' />
+                <button onClick={handleClick} className='bg-blue-600 px-10 text-[23px] py-2 mt-4 rounded-md text-white'>Add</button>
+            </div>
+        </div>
+    )
 }
-
-export default Home
